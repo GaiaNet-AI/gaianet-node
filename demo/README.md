a1e7275d23ccc2e096e97c9952cc8df1ad8e9a4b
Xin Liu <sam@secondstate.io>
2024-03-17 17:27:48 +0900

    feat: add scripts for demo
    Signed-off-by: Xin Liu <sam@secondstate.io>

---
+++ demo/README.md
@@ -0,0 +1,39 @@
+# Demo Scripts
+
+## config.sh
+
+```bash
+./config.sh \
+    --chat https://huggingface.co/second-state/Llama-2-7B-Chat-GGUF/resolve/main/Llama-2-7b-chat-hf-Q5_K_M.gguf \
+    --embedding https://huggingface.co/second-state/All-MiniLM-L6-v2-Embedding-GGUF/resolve/main/all-MiniLM-L6-v2-ggml-model-f16.gguf \
+    --snapshot https://huggingface.co/datasets/gaianet/paris/resolve/main/paris_4096_llama2-7b.snapshot
+```
+
+## init.sh
+
+```bash
+./init.sh
+```
+
+Task list of `init.sh`:
+
+- Install WasmEdge with wasi-nn_ggml + rustls plugins
+- Install Qdrant
+- Download gguf chat model (in `config.json`) to `$HOME/gaia`
+- Download gguf embedding model (in `config.json`) to `$HOME/gaia`
+- Download `llama-api-server.wasm` (Not release version) to `$HOME/gaia`
+- Download `dashboard` to `$HOME/gaia`
+- Create `qdrant` directory in `$HOME/gaia` as the default data directory of Qdrant
+- Recover the snapshot (in `config.json`) to Qdrant
+
+## start.sh
+
+```bash
+./start.sh
+```
+
+Task list of `start.sh`:
+
+- Start Qdrant instance at `http://0.0.0.0:6333` (REST API)
+- Start LlamaEdge API Server at `http://0.0.0.0:8080`
+  - The gaianet branded chatbot-ui at `http://0.0.0.0:8080/index.html`
--- demo/init.sh
+++ demo/init.sh
@@ -1,66 +1,22 @@
-!/bin/bash
+# !/bin/bash
 
 # target name
 target=$(uname -m)
 
-snapshot=""
-collection_name=""
-model_url=""
 
-function print_usage {
-    printf "Usage:\n"
-    printf "  ./install-gaia.sh\n\n"
-    printf "  --snapshot: Url or local path of Qdrant collection snapshot\n"
-    printf "  --collection-name: Name for the recovered collection\n"
-    printf "  --model-url: Url of GGUF model\n"
-    printf "  --help: Print usage\n"
-}
-
-while [[ $# -gt 0 ]]; do
-    key="$1"
-    case $key in
-        --snapshot)
-            snapshot="$2"
-            shift
-            shift
-            ;;
-        --collection-name)
-            collection_name="$2"
-            shift
-            shift
-            ;;
-        --model-url)
-            model_url="$2"
-            shift
-            shift
-            ;;
-        --help)
-            print_usage
-            exit 0
-            ;;
-        *)
-            echo "Unknown argument: $key"
-            print_usage
-            exit 1
-            ;;
-    esac
-done
-
-# check if the model url is specified or not
-if [ -z "$model_url" ]; then
-    printf "Please specify a model url using '--model-url' option\n"
+# check if config.json exists or not
+if [ ! -f "config.json" ]; then
+    printf "config.json file not found\n"
     exit 1
 fi
-
-if [ -n "$snapshot" ] && [ -z "$collection_name" ]; then
-    printf "Please specify a collection name using '--collection-name' option when snapshot is provided\n"
-    exit 1
-fi
+# represents the directory where the script is located
+cwd=$(pwd)
 
 printf "\n"
 
+
 # 1. Install WasmEdge with wasi-nn_ggml plugin for local user
-printf "[+] Installing WasmEdge with wasi-nn_ggml plugin for local user...\n\n"
+printf "[+] Installing WasmEdge with wasi-nn_ggml plugin ...\n\n"
 if curl -sSf https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh | bash -s -- --plugins wasi_nn-ggml wasmedge_rustls; then
     source $HOME/.wasmedge/env
     wasmedge_path=$(which wasmedge)
@@ -73,7 +29,7 @@ fi
 printf "\n"
 
 # 2. Install Qdrant at /home/user/bin/qdrant
-printf "[+] Installing Qdrant...\n"
+printf "[+] Installing Qdrant...\n\n"
 qdrant_version="v1.8.1"
 if [ "$(uname)" == "Darwin" ]; then
     # Check if "$HOME/bin" directory exists
@@ -127,51 +83,67 @@ fi
 # Set "base_dir" to $HOME/gaia
 base_dir="$HOME/gaia"
 
-# 4. Download Mistral 7B GGUF file to $HOME/gaia
-model=$(basename $model_url)
-if [ -f "$base_dir/$model" ]; then
-    printf "[+] Using the cached Mistral-7B-Instruct-v0.2 model file\n"
-else
-    printf "[+] Downloading $model ...\n"
-    curl -L $model_url -o $base_dir/$model
-fi
-printf "\n"
+# 4. Download GGUF chat model to $HOME/gaia
+url_chat_model=$(awk -F'"' '/"chat":/ {print $4}' config.json)
+if [[ $url_chat_model =~ ^https://huggingface\.co/second-state ]]; then
+    chat_model=$(basename $url_chat_model)
 
-# 5. Download chatbot-ui
-if [ -d "$base_dir/chatbot-ui" ]; then
-    printf "[+] Using cached Chatbot web app\n"
-else
-    printf "[+] Downloading Chatbot web app ...\n"
-    cd $base_dir
-    files_tarball="https://github.com/second-state/chatbot-ui/releases/latest/download/chatbot-ui.tar.gz"
-    curl -L $files_tarball -o chatbot-ui.tar.gz
-    if [ $? -ne 0 ]; then
-        printf "    \nFailed to download ui tarball. Please manually download from https://github.com/second-state/chatbot-ui/releases/latest/download/chatbot-ui.tar.gz and unzip the "chatbot-ui.tar.gz" to the current directory.\n"
-        exit 1
+    if [ -f "$base_dir/$chat_model" ]; then
+        printf "[+] Using the cached chat model: $chat_model\n"
+    else
+        printf "[+] Downloading $chat_model ...\n\n"
+        curl -L $url_chat_model -o $base_dir/$chat_model
     fi
-    tar xzf chatbot-ui.tar.gz
-    rm chatbot-ui.tar.gz
+    printf "\n"
+else
+    printf "Error: the chat model is not from https://huggingface.co/second-state\n"
+    exit 1
 fi
-printf "\n"
+
+# 5. Download GGUF embedding model to $HOME/gaia
+url_embedding_model=$(awk -F'"' '/"embedding":/ {print $4}' config.json)
+if [[ $url_embedding_model =~ ^https://huggingface\.co/second-state ]]; then
+    embedding_model=$(basename $url_embedding_model)
+
+    if [ -f "$base_dir/$embedding_model" ]; then
+        printf "[+] Using the cached embedding model: $embedding_model\n"
+    else
+        printf "[+] Downloading $embedding_model ...\n\n"
+        curl -L $url_embedding_model -o $base_dir/$embedding_model
+    fi
+    printf "\n"
+else
+    printf "Error: the embedding model is not from https://huggingface.co/second-state\n"
+    exit 1
+fi
 
 # 6. Download llama-api-server.wasm
-printf "[+] Downloading the latest llama-api-server.wasm ...\n"
+printf "[+] Downloading the llama-api-server.wasm ...\n\n"
 cd $base_dir
-curl -LO https://github.com/LlamaEdge/LlamaEdge/releases/latest/download/llama-api-server.wasm
+curl -LO https://github.com/LlamaEdge/LlamaEdge/raw/feat-server-multi-models/api-server/llama-api-server.wasm
 printf "\n"
 
-# 7. Download create_embeddings.wasm
-printf "[+] Downloading the latest create_embeddings.wasm ...\n"
-cd $base_dir
-curl -LO https://github.com/YuanTony/chemistry-assistant/raw/main/rag-embeddings/create_embeddings.wasm
+# 7. Download dashboard to $HOME/gaia
+if [ -d "$base_dir/dashboard" ]; then
+    printf "[+] Using cached dashboard ...\n"
+else
+    printf "[+] Downloading dashboard ...\n"
+    if [ -d "$base_dir/gaianet-node" ]; then
+        rm -rf $base_dir/gaianet-node
+    fi
+    git clone -q https://github.com/GaiaNet-AI/gaianet-node.git $base_dir/gaianet-node
+    cp -r $base_dir/gaianet-node/dashboard $base_dir
+    rm -rf $base_dir/gaianet-node
+fi
 printf "\n"
 
-
-# 8. prepare qdrant
+# 8. prepare qdrant dir if it does not exist
 if [ ! -d "$base_dir/qdrant" ]; then
+    printf "[+] Preparing Qdrant directory ...\n"
     mkdir -p $base_dir/qdrant && cd $base_dir/qdrant
 
-    curl -LO https://github.com/qdrant/qdrant/archive/refs/tags/v1.8.1.tar.gz
+    # download qdrant binary
+    curl -s -LO https://github.com/qdrant/qdrant/archive/refs/tags/v1.8.1.tar.gz
     # unzip to `qdrant-1.8.1` directory
     tar -xzf v1.8.1.tar.gz
     rm v1.8.1.tar.gz
@@ -199,7 +171,6 @@ if [ ! -d "$base_dir/qdrant" ]; then
     # remove the `qdrant-1.8.1` directory
     rm -rf qdrant-1.8.1
 
-
     # start qdrant to create the storage directory structure if it does not exist
     cd $base_dir/qdrant
     nohup qdrant > /dev/null 2>&1 &
@@ -211,28 +182,33 @@ if [ ! -d "$base_dir/qdrant" ]; then
 fi
 
 # 9. recover from the given qdrant collection snapshot
-if [ ! -z "$snapshot" ]; then
+printf "[+] Recovering the given Qdrant collection snapshot ...\n"
+cd $cwd
+url_snapshot=$(awk -F'"' '/"snapshot":/ {print $4}' config.json)
+collection_name=$(basename $url_snapshot)
 
-    cd $base_dir/qdrant
+# start qdrant
+cd $base_dir/qdrant
+nohup qdrant > /dev/null 2>&1 &
+sleep 2
+qdrant_pid=$!
 
-    # start qdrant
-    nohup qdrant > /dev/null 2>&1 &
-    sleep 2
-    qdrant_pid=$!
+response=$(curl -X PUT http://localhost:6333/collections/$collection_name/snapshots/recover \
+    -H "Content-Type: application/json" \
+    -d "{\"location\":\"$url_snapshot\", \"priority\": null, \"checksum\": null}")
+sleep 5
 
-    response=$(curl -X PUT http://localhost:6333/collections/$collection_name/snapshots/recover \
-        -H "Content-Type: application/json" \
-        -d "{\"location\":\"$snapshot\", \"priority\": null, \"checksum\": null}")
-    sleep 5
+printf "\n"
 
-    printf "\n"
-
-    if echo "$response" | grep -q '"status":"ok"'; then
-        printf "Recovery from the collection snapshot is done.\n\n"
-    else
-        printf "Failed to recover from the collection snapshot.\n\n"
-    fi
-
-    # stop qdrant
-    kill $qdrant_pid
+if echo "$response" | grep -q '"status":"ok"'; then
+    printf "Recovery from the collection snapshot is done.\n\n"
+else
+    printf "Failed to recover from the collection snapshot.\n\n"
 fi
+
+# stop qdrant
+kill $qdrant_pid
+
+printf "\n>>> The gaia environment is ready. To start the gaia services, run the command: ./start.sh <<<\n"
+
+exit 0
