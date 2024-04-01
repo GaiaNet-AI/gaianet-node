24b2142788bdbd446ce169f790c66ea20b8e18e0
Michael Yuan <michael@michaelyuan.com>
2024-04-01 13:53:01 -0500

    More robust termination of the process

--- demo/stop.sh
+++ demo/stop.sh
@@ -3,20 +3,20 @@
 # stop the Qdrant instance
 if [ -f "qdrant.pid" ]; then
     printf "[+] Stopping Qdrant instance ...\n"
-    kill $(cat qdrant.pid)
+    kill -9 $(cat qdrant.pid)
     rm qdrant.pid
 fi
 
 # stop the api-server
 if [ -f "llamaedge.pid" ]; then
     printf "[+] Stopping API server ...\n"
-    kill $(cat llamaedge.pid)
+    kill -9 $(cat llamaedge.pid)
     rm llamaedge.pid
 fi
 
 # stop gaianet-domain
 if [ -f "gaianet-domain.pid" ]; then
     printf "[+] Stopping gaianet-domain ...\n"
-    kill $(cat gaianet-domain.pid)
+    kill -9 $(cat gaianet-domain.pid)
     rm gaianet-domain.pid
 fi
