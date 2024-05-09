17a585d438b81f1640c244fc1cdc4dd3528bbe85
JYC0413 <juyichen0413@foxmail.com>
2024-05-09 12:02:36 +0000

    Build and deploy chatbot-ui

---
+++ dashboard/_next/static/8qUKtt6uandF05HWqq6xX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/542b50fd-07ebedc579cef971.js","static/chunks/994-b9b24b123e1aeb75.js","static/chunks/pages/index-9d9f0f1b881be974.js"],"/_error":["static/chunks/pages/_error-3f6d1c55bb8051ab.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
