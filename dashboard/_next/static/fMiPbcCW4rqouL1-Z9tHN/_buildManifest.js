65a75f6f032f69bd1bfd602eef22efade31a5ecf
Michael Yuan <michael@secondstate.io>
2024-06-21 07:12:29 +0000

    Use a versioned dashboard release from the chatbot-ui repo
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- dashboard/_next/static/fMiPbcCW4rqouL1-Z9tHN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/542b50fd-07ebedc579cef971.js","static/chunks/597-61c76a58e540fce4.js","static/chunks/pages/index-8886e0c68824576b.js"],"/_error":["static/chunks/pages/_error-3f6d1c55bb8051ab.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
