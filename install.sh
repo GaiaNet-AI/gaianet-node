49f2f2c743a1bdb3c850de2c4e4a68b26fe3a925
Xin Liu <sam@secondstate.io>
2024-12-06 19:35:36 +0800

    version: bump to `0.4.13`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ target=$(uname -m)
 cwd=$(pwd)
 
 repo_branch="main"
-version="0.4.12"
-rag_api_server_version="0.9.15"
-llama_api_server_version="0.14.15"
+version="0.4.13"
+rag_api_server_version="0.9.17"
+llama_api_server_version="0.14.17"
 wasmedge_version="0.14.1"
-ggml_bn="b3651"
+ggml_bn="b4273"
 vector_version="0.38.0"
 dashboard_version="v3.1"
 assistant_version="0.3.2"
