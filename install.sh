c65e104c9e56d690150dd01969de58bf9e477b62
Xin Liu <sam@secondstate.io>
2025-01-24 11:11:17 +0800

    chore: upgrade `server-assistant` to `0.4.0`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ wasmedge_version="0.14.1"
 ggml_bn="b4419"
 vector_version="0.38.0"
 dashboard_version="v3.1"
-assistant_version="0.3.6"
+assistant_version="0.4.0"
 qdrant_version="v1.11.4"
 
 # 0: do not reinstall, 1: reinstall
