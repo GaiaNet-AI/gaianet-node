68cf9d274d9083a1e0905aced3e6e22271dd1c45
Xin Liu <sam@secondstate.io>
2025-02-10 16:06:20 +0800

    version: bump to `0.4.19`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ target=$(uname -m)
 cwd=$(pwd)
 
 repo_branch="main"
-version="0.4.18"
+version="0.4.19"
 rag_api_server_version="0.13.4"
 llama_api_server_version="0.16.6"
 wasmedge_version="0.14.1"
