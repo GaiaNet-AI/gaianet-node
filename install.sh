4d63b77fc7b241951afc11674b04cf1c1c25c034
Xin Liu <sam@secondstate.io>
2025-05-16 17:54:17 +0800

    chore: bump llama-api-server to `0.18.4`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ target=$(uname -m)
 # represents the directory where the script is located
 cwd=$(pwd)
 
-repo_branch="main"
+repo_branch="ci-ggml-b5361"
 version="0.5.0"
-llama_api_server_version="0.17.0"
+llama_api_server_version="0.18.4"
 gaia_nexus_version="0.1.0"
 wasmedge_version="0.14.1"
 ggml_bn="b5361"
@@ -589,9 +589,8 @@ fi
 # 7. Download LlamaEdge API server
 printf "[+] Downloading LlamaEdge API server ...\n"
 # download llama-api-server.wasm
-# check_curl https://github.com/LlamaEdge/LlamaEdge/releases/download/$llama_api_server_version/llama-api-server.wasm $gaianet_base_dir/llama-api-server.wasm
-# check_curl https://github.com/GaiaNet-AI/gaia-nexus-release/releases/download/$gaia_nexus_version/llama-api-server.wasm $gaianet_base_dir/llama-api-server.wasm
-check_curl https://github.com/GaiaNet-AI/gaianet-node/releases/download/$version/llama-api-server.wasm $gaianet_base_dir/llama-api-server.wasm
+check_curl https://github.com/LlamaEdge/LlamaEdge/releases/download/$llama_api_server_version/llama-api-server.wasm $gaianet_base_dir/llama-api-server.wasm
+# check_curl https://github.com/GaiaNet-AI/gaianet-node/releases/download/$version/llama-api-server.wasm $gaianet_base_dir/llama-api-server.wasm
 
 info "    👍 Done! The llama-api-server.wasm is downloaded in $gaianet_base_dir"
 
