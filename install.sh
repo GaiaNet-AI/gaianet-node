b486aa567564143a1d30b167881f46caddb58f1e
Xin Liu <sam@secondstate.io>
2025-08-31 10:39:37 +0800

    chore: bump `llama_api_server_version` and `gaia_nexus_version`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ cwd=$(pwd)
 
 repo_branch="feat-new-nexus"
 version="0.6.0"
-llama_api_server_version="0.25.1"
-gaia_nexus_version="0.2.0"
+llama_api_server_version="0.26.1"
+gaia_nexus_version="0.8.0"
 wasmedge_version="0.14.1"
 ggml_bn="b6097"
 vector_version="0.38.0"
